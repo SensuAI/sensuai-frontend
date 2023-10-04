bb740672d049e07121efb6e73420582a182f5435
JorgeBlancoA01745907 <99824154+JorgeBlancoA01745907@users.noreply.github.com>
2023-10-03 21:36:21 -0600

    03/10/23
    v2

--- app/(auth)/test/page.tsx
+++ app/(auth)/test/page.tsx
@@ -1,15 +1,56 @@
 'use client';
 import * as React from 'react';
 import * as Form from '@radix-ui/react-form';
-import SucursalForm from '@/components/form/SucursalForm';
 
 
 function Test() {
 
+    function submitForm(data: any) {
+        console.log(data);
+    }
+    const [serverErrors, setServerErrors] = React.useState({
+        email: false,
+        password: false,
+    });
     return (
-    <main>
-        <SucursalForm/>
-    </main>
+        <Form.Root
+            // `onSubmit` only triggered if it passes client-side validation
+            onSubmit={(event) => {
+                const data = Object.fromEntries(new FormData(event.currentTarget));
+                // Submit form data and catch errors in the response
+                submitForm(data)    
+                // prevent default form submission
+                event.preventDefault();
+            }}
+            onClearServerErrors={() =>
+                setServerErrors({ email: false, password: false })
+            }
+        >
+            <Form.Field name="email" serverInvalid={serverErrors.email}>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" required />
+                <Form.Message match="valueMissing">
+                    Please enter your email.
+                </Form.Message>
+                <Form.Message match="typeMismatch" forceMatch={serverErrors.email}>
+                    Please provide a valid email.
+                </Form.Message>
+            </Form.Field>
+            <Form.Field name="password" serverInvalid={serverErrors.password}>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" required />
+                <Form.Message match="valueMissing">
+                    Please enter a password.
+                </Form.Message>
+                {serverErrors.password && (
+                    <Form.Message>
+                        Please provide a valid password. It should contain at least 1 number
+                        and 1 special character.
+                    </Form.Message>
+                )}
+            </Form.Field>
+            <Form.Submit>Submit</Form.Submit>
+        </Form.Root>
     );
 }
 export default Test;
