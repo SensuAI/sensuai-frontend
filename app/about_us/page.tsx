4b7ccaac5ab718eee05c8bbd8c129808cde9e915
JorgeBlancoA01745907 <99824154+JorgeBlancoA01745907@users.noreply.github.com>
2023-11-16 23:08:43 -0600

    About us changes
    16/11/23

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -11,37 +11,53 @@ import oxxocomb from "../assets/oxxocomb.jpg";
 
 const AboutUsPage: React.FC = () => {
   return (
-    <main>
-      <BackgroundSVG/>
-      <Flex justify="center"  direction="column">
-        <AboutInfo
-            image={oxxoeyye}
-            title=""
-          />
-        <Card variant='ghost' style={{ height: 500, width: 1050 }}>
-            <Flex justify="center" mt="4">
-              <Heading highContrast color="sky" size="8">¿Cómo funciona?</Heading>
-            </Flex>
-            <Flex gap="4" justify="center" mt="5" px="9" width="auto" mr="9" ml="9" direction="column">
-              <Text highContrast color="sky" align="center" size="4">Con ayuda de las cámaras ubicadas en cada una de las sucursales <Strong>se puede identificar a los clientes que visitan las tiendas</Strong>, así como su frecuencia de visita y el tiempo que permanecen en estas. Basado en estos datos, <Strong>el modelo genera sugerencias de promociones</Strong> ser ofrecidas a los clientes.</Text>
-            </Flex>
-            <Flex justify="center" mt="6">
-              <Image
-                style={{
-                  borderRadius: '8px',
-                  border: '1px solid crimson'
-                }}
-                src={oxxocomb}
-                width={600}
-                height={600}
-                alt="House Example"
-                className="rounded-lg" />
-            </Flex>
-            
-      </Card>
-      </Flex>
+    <main className="flex flex-col items-center justify-center w-full h-full">
 
-    </main>
+  <BackgroundSVG />
+  
+  <Flex justify="center" direction="column">
+    <AboutInfo image={oxxoeyye} title="" />
+
+    <Card variant='classic' style={{ height: 500, width: 1050, margin: "1rem", marginLeft: "8rem" }} className="mt-8 flex flex-col items-center">
+      <Flex justify="center" mt="4">
+        <Heading highContrast color="sky" size="8">
+          ¿Cómo funciona?
+        </Heading>
+      </Flex>
+      <Flex
+        gap="4"
+        justify="center"
+        mt="5"
+        px="9"
+        width="auto"
+        mr="9"
+        ml="9"
+        direction="column"
+      >
+        <Text highContrast color="sky" align="center" size="4">
+          Con ayuda de las cámaras ubicadas en cada una de las sucursales{' '}
+          <Strong>se puede identificar a los clientes que visitan las tiendas</Strong>, así como su frecuencia de visita y el tiempo que permanecen en estas. Basado en estos datos,{' '}
+          <Strong>el modelo genera sugerencias de promociones</Strong> ser ofrecidas a los clientes.
+        </Text>
+      </Flex>
+      <Flex justify="center" mt="6">
+        <Image
+          style={{
+            borderRadius: '8px',
+            border: '1px solid crimson',
+          }}
+          src={oxxocomb}
+          width={600}
+          height={600}
+          alt="House Example"
+          className="rounded-lg"
+        />
+      </Flex>
+    </Card>
+  </Flex>
+</main>
+
+
   );
 };
 
